8f569aa24540f90b669a62f783f0251c3f38731d
Reinaldo Reyes <reinaldo.reyes@correo.unimet.edu.ve>
2024-06-26 23:36:16 -0400

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import LoginForm from './pages/LoginForm.jsx'
+import RegisterForm from './pages/RegisterForm.jsx'
+import LandingPage from './pages/LandingPage.jsx'
+function App() {
+
+  return (
+    <>
+    <LandingPage/>
+    </>
+  )
+}
+
+export default App
