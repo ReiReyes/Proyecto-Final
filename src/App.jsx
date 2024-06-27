49f5414aef2361ab80f536fbebe24f8fd060f972
Reinaldo Reyes <reinaldo.reyes@correo.unimet.edu.ve>
2024-06-27 11:59:00 -0400

    inicio-admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import LoginForm from './pages/LoginForm.jsx'
 import RegisterForm from './pages/RegisterForm.jsx'
 import LandingPage from './pages/LandingPage.jsx'
+import AdminMenu from './pages/AdminMenu.jsx'
 function App() {
 
   return (
     <>
-    <LandingPage/>
+    <AdminMenu/>
     </>
   )
 }
---
+++ src/pages/AdminMenu.jsx
@@ -0,0 +1,15 @@
+
+import "../assets/styles/AdminMenu.css"
+
+import Headerp from "../components/Header-p"
+function AdminMenu(){
+    return(
+        <div className="bodyAMenu">
+
+        <Headerp/>
+        <h1>Welcome to admin menu</h1>
+        </div>
+    );
+}
+
+export default AdminMenu
