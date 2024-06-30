96f80b2bdaa47b98c28926cfac63f7564ae67d0d
Truenno547YT <d.andres@correo.unimet.edu.ve>
2024-06-30 18:18:20 -0400

    Implementacion de cambiar datos generales de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from './pages/LoginForm.jsx'
 import RegisterForm from './pages/RegisterForm.jsx'
 import LandingPage from './pages/LandingPage.jsx'
@@ -6,15 +7,36 @@ import AdminHist from './pages/AdminHist.jsx'
 import Profile from './pages/Profile.jsx'
 import Profile_Sec from './pages/Profile_Sec.jsx'
 import AdminPromo from './pages/AdminPromo.jsx'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
+import { auth } from "./pages/firebase.js";
+
 
 
 function App() {
 
+ const [user, setUser]=useState();
+  useEffect(()=>{
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    }); 
+  });
+
+
   return (
     <>
-    <LoginForm/>
+    <BrowserRouter>        
+      <Routes>
+        <Route index element={user? <Navigate to ="/Profile"/> : <LoginForm />} />
+        <Route path="/Login" element = {<LoginForm />} />
+        <Route path="/Profile" element = {<Profile />} />
+        <Route path="/LandingPage" element = {<LandingPage/>} />
+        <Route path="/register" element = {<RegisterForm/>} />
+      </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
-
 export default App
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,37 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { auth, db } from "./firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import '../assets/styles/Profile.css';
 import Header from '../components/Header-p';
-import ImgUser from '../assets/imgs/imgUser/no_perfil.png'
-import change_pfp from '../assets/imgs/imgUser/change_pfp.png'
+import ImgUser from '../assets/imgs/imgUser/no_perfil.png';
+import change_pfp from '../assets/imgs/imgUser/change_pfp.png';
 
 function Profile() {
+  const [userDetails, setUserDetails] = useState(null);
+  const [telf, setTelf] = useState("");
+  const [rol, setRol] = useState("");
+  const [name, setName] = useState("");
+  const [last_name, setLast_name] = useState("");
 
+  const fetchUserData = async () => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const docRef = doc(db, "Users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          setUserDetails(data);
+          setTelf(data.telefono || "");
+          setRol(data.rol || "");
+          setName(data.nombre || "");
+          setLast_name(data.apellido || "");
+        } else {
+          console.log("Usuario no esta loggeado");
+        }
+      }
+    });
+  };
 
+  useEffect(() => {
+    fetchUserData();
+  }, []);
 
+  const handleSave = async (event) => {
+    event.preventDefault();
+    console.log("HandleSave called");
+    const change = doc(db, "Users", auth.currentUser.uid);
+    await updateDoc(change, {
+      nombre: name,
+      apellido: last_name,
+      rol: rol,
+      telefono: telf,
+    });
+    console.log("Changes saved");
+  };
+
+  if (!userDetails) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-    <div className='bodyProfile'>
-    <Header primero= "Perfil" segundo= "Metodos y Seguridad" tercero= "Historial" cuarto= "Log Out"/>
-        <div className='User_Profile'>
-            <img className="Img_user" src={ImgUser}></img>
-            <img className="Img_change" src={change_pfp}></img>
-          <button className='profile_save_button' >Guardar Cambios</button>
+      <form onSubmit={handleSave}>
+        <div className='bodyProfile'>
+          <Header primero="Perfil" segundo="Metodos y Seguridad" tercero="Historial" cuarto="Log Out" />
+          <div className='User_Profile'>
+            <img className="Img_user" src={ImgUser} alt="User" />
+            <img className="Img_change" src={change_pfp} alt="Change" />
+            <button type="submit" className='profile_save_button'>Guardar Cambios</button>
+          </div>
+          <div className='container_user_options'>
+            <h2 className='titulo_profile'>Nombre</h2>
+            <input className='item_profile' value={name} onChange={(e) => setName(e.target.value)} />
+            <div className='titulo_profile'>Apellido</div>
+            <input className='item_profile' value={last_name} onChange={(e) => setLast_name(e.target.value)} />
+            <div className='titulo_profile'>Rol Universitario</div>
+            <input className='item_profile' value={rol} onChange={(e) => setRol(e.target.value)} />
+            <div className='titulo_profile'>Telefono</div>
+            <input className='item_profile' value={telf} onChange={(e) => setTelf(e.target.value)} />
+            <button type="submit" className='profile_save_button_responsive'>Guardar Cambios</button>
+          </div>
         </div>
-        <div className='container_user_options'>
-          <h2 className='titulo_profile'>Nombre</h2>
-          <input className='item_profile' placeholder='hi' />
-          <div className='titulo_profile'>Apellido</div>
-          <input className='item_profile' placeholder='hallo' />
-          <div className='titulo_profile'>Rol Universitario</div>
-          <input className='item_profile' placeholder='hallo' />
-          <div className='titulo_profile'>Telefono</div>
-          <input className='item_profile' placeholder='hallo' />
-          <button className='profile_save_button_responsive' >Guardar Cambios</button>
-        </div>
-    </div>
+      </form>
     </>
   );
-
 }
 
 export default Profile;
