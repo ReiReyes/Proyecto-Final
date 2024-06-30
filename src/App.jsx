19560b06e7bca2097bd000d67b84572158d79cf9
Truenno547YT <d.andres@correo.unimet.edu.ve>
2024-06-29 20:33:29 -0400

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-    <AdminPromo/>
+    <Profile_Sec/>
     </>
   )
 }
--- src/pages/Profile_Sec.jsx
+++ src/pages/Profile_Sec.jsx
@@ -9,17 +9,17 @@ function Profile_Sec() {
 const [cards] = useState([
   {
     title: 'Contraseña',
-    text: 'yo hello guys its asian guy streamer here',
+    text: 'Cambiar Contraseña',
   },
   {
-    title: 'Correo Electronico',
+    title: 'Paypal',
     text: 'yo hello guys its asian guy streamer here',
   },
 ]);
 
 const [cards2] = useState([
   {
-    title: 'Paypal',
+    title: 'Correo Electronico',
     text: 'yo hello guys its asian guy streamer here',
   },
   {
@@ -32,15 +32,17 @@ const [cards2] = useState([
     <div className='bodyProfile_Sec'>
     <Header primero= "Perfil" segundo= "Metodos y Seguridad" tercero= "Historial" cuarto= "Log Out"/>
     <section className='section_options'>
-        <div className='card_box_1'>
+        <div className='card_box'>
             <div className='cards'>      
             {
                 cards.map((card, i) => (
-            
+
+          <div className='cards'>
+              <h3 className='titles_security'>{card.title}</h3>
             <div key={i} className='card'>
-                <p>{card.text}</p>
-                <p> gogo tata</p>
+                <button>{card.text}</button>
             </div>
+          </div>
             ))
         }
         </div>
@@ -49,12 +51,13 @@ const [cards2] = useState([
             <div className='cards'>      
             {
                 cards2.map((card, i) => (
-            
+
+          <div className='cards'>
+              <h3 className='titles_security'>{card.title}</h3>
             <div key={i} className='card'>
-                <h1>{card.title}</h1>
                 <p>{card.text}</p>
-                <p> gogo tata</p>
             </div>
+          </div>
             ))
         }
         </div>
