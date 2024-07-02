694b23088b1ba1bbc6a5d4757ea02f4ddc2ea857
JuanMartin3 <145402763+JuanMartin3@users.noreply.github.com>
2024-07-01 20:10:48 -0400

    fix products.js/ nuevos productos

---
+++ products.js
@@ -0,0 +1,194 @@
+import product1 from './src/assets/imgs/imgEntradas/CEVICHE.png'
+import product2 from './src/assets/imgs/imgEntradas/MOLOKAI.png'
+import product3 from './src/assets/imgs/imgEntradas/WAKAME.png'
+import product4 from './src/assets/imgs/imgBowls/TASTY SUNSET.png'
+import product5 from './src/assets/imgs/imgBowls/Tropical Sun.png'
+import product6 from './src/assets/imgs/imgBowls/Shrimp Affair.png'
+import product7 from './src/assets/imgs/imgBowls/Tuna Lover.png'
+import product8 from './src/assets/imgs/imgBurritos/Mololove.png'
+import product9 from './src/assets/imgs/imgBurritos/CrazyOasis.png'
+import product10 from './src/assets/imgs/imgBurritos/Kilauea.png'
+import product11 from './src/assets/imgs/imgBurritos/FunkyTuna.png'
+import product12 from './src/assets/imgs/imgBurritos/SalmonTherapy.png'
+import product13 from './src/assets/imgs/imgBebidas/7Up.png'
+import product14 from './src/assets/imgs/imgBebidas/Gatorade.png'
+import product15 from './src/assets/imgs/imgBebidas/Lipton.png'
+import product16 from './src/assets/imgs/imgBebidas/MaltinPolar.png'
+import product17 from './src/assets/imgs/imgBebidas/Minalba.png'
+import product18 from './src/assets/imgs/imgBebidas/Pepsi.png'
+import product19 from './src/assets/imgs/imgBebidas/PepsiLight.png'
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Ceviche",
+        price: 15.00,
+        productImage: product1,
+        productDesc: "KANI, MASAGO, ATÚN,MAYO SPICY, SALMÓN,SALSA ANGUILA, WAKAME.",
+        productType: "entrada",
+    },
+
+    {
+        id: 2,
+        productName: "Molokai Ensalada",
+        price: 10.00,
+        productImage: product2,
+        productDesc: "WAKAME, MASAGO,SEMILLA AJONKOLÍ",
+        productType: "entrada",
+    },
+
+    {
+        id: 3,
+        productName: "Wakame",
+        price: 13.00,
+        productImage: product3,
+        productDesc: "LIMÓN, CAMARÓN,CEBOLLA MORADA,AJÍ, CILANTRO.",
+        productType: "entrada",
+    },
+
+    {
+        id: 4,
+        productName: "TASTY SUNSET",
+        price: 17.00,
+        productImage: product4,
+        productDesc: "BASE, PIÑA, ATÚN,WAKAME, ZANAHORIA,CRUNCHY.",
+        productType: "bowl",
+    },
+
+    {
+        id: 5,
+        productName: "Tropical Sun",
+        price: 18.00,
+        productImage: product5,
+        productDesc: "BASE, REPOLLO MOR,SALMÓN, EDAMAME,KANI DINAMITA, CRUNCHY.",
+        productType: "bowl",
+    },
+
+    {
+        id: 6,
+        productName: "Shrimp Affair",
+        price: 17.00,
+        productImage: product6,
+        productDesc: "BASE, CEBOLLA MOR,CAMARÓN, AGUACATE, CEBOLLIN, CRUNCHY.",
+        productType: "bowl",
+    },
+
+    {
+        id: 7,
+        productName: "Tuna Lover",
+        price: 18.00,
+        productImage: product7,
+        productDesc: "BASE, PEPINO,ATÚN SPICY, EDAMAME,KANI DINAMITA, CRUNCHY.",
+        productType: "bowl",
+    },
+
+    {
+        id: 8,
+        productName: "Mololove",
+        price: 16.00,
+        productImage: product8,
+        productDesc: "BASE, ATÚN, SALMÓN,CAMARÓN, EDAMAME,ZANAHORIA, KANI, AGUACATE.",
+        productType: "burrito",
+    },
+
+    {
+        id: 9,
+        productName: "Crazy Oasis",
+        price: 14.00,
+        productImage: product9,
+        productDesc: "BASE, ATÚN, SALMÓN, PIÑA, EDAMAME, ZANAHORIA,KANI DIMANIMTA.",
+        productType: "burrito",
+    },
+
+    {
+        id: 10,
+        productName: "Kilauea",
+        price: 15.00,
+        productImage: product10,
+        productDesc: "BASE, CAMARÓN, ATÚN SPICY, CEBILLÍN, RÁBANO, KANI, MASAGO.",
+        productType: "burrito",
+    },
+
+    {
+        id: 11,
+        productName: "Funky Tuna",
+        price: 15.00,
+        productImage: product11,
+        productDesc: "BASE, ATÚN X2, PEPINO, EDAMAME, KANI, AGUACATE, SEMILLA AJONJOLÍ.",
+        productType: "burrito",
+    },
+
+    {
+        id: 12,
+        productName: "Salmon Therapy",
+        price: 13.00,
+        productImage: product12,
+        productDesc: "BASE, SALMÓN X2, PIÑA, PEPINO, REPOLLO MORADO, WAKAME.",
+        productType: "burrito",
+    },
+
+    {
+        id: 13,
+        productName: "7Up",
+        price: 2.0,
+        productImage: product13,
+        productDesc: "",
+        productType: "bebida",
+    },
+
+    {
+        id: 14,
+        productName: "Gatorade",
+        price: 3.00,
+        productImage: product14,
+        productDesc: "",
+        productType: "bebida",
+    },
+
+    {
+        id: 15,
+        productName: "Lipton",
+        price: 2.00,
+        productImage: product15,
+        productDesc: "",
+        productType: "bebida",
+    },
+
+    {
+        id: 16,
+        productName: "Maltin Polar",
+        price: 2.00,
+        productImage: product16,
+        productDesc: "",
+        productType: "bebida",
+    },
+
+    {
+        id: 17,
+        productName: "Agua Minalba",
+        price: 1.00,
+        productImage: product17,
+        productDesc: "",
+        productType: "bebida",
+    },
+
+    {
+        id: 18,
+        productName: "Pepsi",
+        price: 2.00,
+        productImage: product18,
+        productDesc: "",
+        productType: "bebida",
+    },
+
+    {
+        id: 19,
+        productName: "Pepsi Light",
+        price: 2.00,
+        productImage: product19,
+        productDesc: "",
+        productType: "bebida",
+    },
+    
+]
