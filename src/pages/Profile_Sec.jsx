e7ddf7a0f9b4c05e1b47d95311bf71d0dad09553
Truenno547YT <d.andres@correo.unimet.edu.ve>
2024-07-03 19:28:56 -0400

    responsive, adicion de paypal

--- src/pages/Profile_Sec.jsx
+++ src/pages/Profile_Sec.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import '../assets/styles/Profile_Sec.css';
 import Header from '../components/Header-p';
-import Change_Pass from '../components/Change_Pass'
+import Change_Pass from '../components/Change_Pass';
 import { updatePassword } from 'firebase/auth';
-import { auth, db } from "./firebase";
-
+import { auth, db, storage } from "./firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 function Profile_Sec() {
-
   const [newPassword, setNewPassword] = useState("");
-const [showModal, setShowModal] = useState(false);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [showPaypalModal, setShowPaypalModal] = useState(false);
 
-const handleModalOpen = () => setShowModal(true);
-  const handleModalClose = () => setShowModal(false);
+  const handleModalPasswordOpen = () => setShowPasswordModal(true);
+  const handleModalPasswordClose = () => setShowPasswordModal(false);
+  const handleModalPaypalOpen= () => setShowPaypalModal(true);
+  const handleModalPaypalClose = () => setShowPaypalModal(false);
+
+  const [PaypalUser, setPaypalUser] = useState("Usuario")
+  const [PaypalPassword, setPaypalPassword] = useState("Contraseña")
 
   const handleChangePassword = async (event) => {
     event.preventDefault();
@@ -21,22 +26,22 @@ const handleModalOpen = () => setShowModal(true);
     }
     try {
       await updatePassword(auth.currentUser, newPassword);
-      setShowModal(false);
+      setShowPasswordModal(false);
     } catch (error) {
       console.error("Error updating password: ", error);
     }
   };
 
-
-
   const [cards] = useState([
     {
       title: 'Contraseña',
       text: 'Cambiar',
+      button: handleModalPasswordOpen,
     },
     {
       title: 'Pago en Linea',
       text: 'Paypal',
+      button: handleModalPaypalOpen,
     },
   ]);
 
@@ -78,6 +83,17 @@ const handleModalOpen = () => setShowModal(true);
     }));
   };
 
+  const handleSave_Paypal = async (event) => {
+    event.preventDefault();
+    const change = doc(db, "Users", auth.currentUser.uid);
+    await updateDoc(change, {
+    usuario_paypal: PaypalUser,
+    password_paypal: PaypalPassword,
+    });
+    console.log("Changes saved");
+  };
+
+
   return (
     <>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
@@ -90,7 +106,7 @@ const handleModalOpen = () => setShowModal(true);
                 <div key={i} className='cards'>
                   <h3 className='titles_security'>{card.title}</h3>
                   <div className='card'>
-                    <button className='cards_button' onClick={handleModalOpen}>{card.text}</button>
+                    <button className='cards_button' onClick={card.button}>{card.text}</button>
                   </div>
                 </div>
               ))}
@@ -135,7 +151,7 @@ const handleModalOpen = () => setShowModal(true);
           </div>
         </section>
       </div>
-      <Change_Pass show={showModal} handleClose={handleModalClose}>
+      <Change_Pass show={showPasswordModal} handleClose={handleModalPasswordClose} operation="Cambiar Contraseña">
         <form onSubmit={handleChangePassword}>
           <div>
             <input
@@ -146,7 +162,26 @@ const handleModalOpen = () => setShowModal(true);
             />
           </div>
           <button type="submit" className="password_change_button">Guardar</button>
+        </form>
+      </Change_Pass>
 
+      <Change_Pass show={showPaypalModal} handleClose={handleModalPaypalClose} operation= "Paypal">
+        <form onSubmit={handleSave_Paypal}>
+          <div>
+            <input
+              type="user"
+              value={PaypalUser}
+              onChange={(e) => setPaypalUser(e.target.value)}
+              className="password_change_input"
+            />
+            <input
+              type="password"
+              value={PaypalPassword}
+              onChange={(e) => setPaypalPassword(e.target.value)}
+              className="password_change_input"
+            />
+          </div>
+          <button type="submit" className="password_change_button">Guardar</button>
         </form>
       </Change_Pass>
     </>
