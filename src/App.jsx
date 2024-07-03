eeaaceddc8ab78c3d93b69c4ff1b50a9227060b5
Truenno547YT <d.andres@correo.unimet.edu.ve>
2024-07-03 19:53:56 -0400

    arreglo final de conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./pages/LoginForm.jsx";
 import RegisterForm from "./pages/RegisterForm.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
@@ -8,6 +9,7 @@ import AdminPromo from "../src/pages/AdminPromo.jsx"
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Profile from "./pages/Profile.jsx";
+import Profile_Sec from "./pages/Profile_Sec.jsx";
 import { useEffect, useState } from "react";
 import { auth } from "./pages/firebase.js";
 import Carro from "../src/pages/Menu/Carro.jsx";
@@ -25,22 +27,14 @@ function App() {
     }); 
   });
 
- const [user, setUser]=useState();
-  useEffect(()=>{
-    auth.onAuthStateChanged((user) => {
-      setUser(user);
-    }); 
-  });
-
-
   return (
         <ShopContextProvider>
           <BrowserRouter>
               <Routes>
-                <Route index element={user ? <Navigate to='/Profile' /> : <LoginForm />} />
                 <Route path="/Login" element={<LoginForm />} />
                 <Route path="/Register" element={<RegisterForm />} />
                 <Route path="/Profile" element={<Profile />} />
+                <Route path="/Profile_security" element={<Profile_Sec />} />
                 <Route path="/LandingPage" element={<LandingPage />} />
                 <Route path="Manage-Menu" element={<AdminMenu/>}/>
                 <Route path="Manage-Promos" element={<AdminPromo/>}/>
