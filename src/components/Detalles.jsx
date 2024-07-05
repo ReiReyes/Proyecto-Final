f93e950d696941b95218a98ee5ab6173d7653443
LuisARRIETA2004 <122703126+LuisARRIETA2004@users.noreply.github.com>
2024-07-04 22:15:07 -0400

    Funcion de hora en Detalles

--- src/components/Detalles.jsx
+++ src/components/Detalles.jsx
@@ -1,21 +1,39 @@
+import React, { useState } from 'react';
 import "../assets/styles/Detalles.css";
 import exit from "../assets/imgs/imgDetalles/Exit.png";
 import delivery from "../assets/imgs/imgDetalles/imagenDelivery.png";
 import PropTypes from 'prop-types';
 
-
 const Detalles = ({ onNext }) => {
+  const [horaEntrega, setHoraEntrega] = useState('2pm');
+
+  const handleHoraClick = () => {
+    
+    // Se que debe haber una forma muchisimo mas optimizada,
+    // pero tendre esta mientras sigo con los demas ajustes
+    const horas = ['8am', '9am', '10am', '11am', '12pm',
+       '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+    const index = horas.indexOf(horaEntrega);
+    
+    if (index === 10) {
+      setHoraEntrega('8am');
+    } else if (index === 11) {
+      setHoraEntrega('8am');
+    } else if (index >= 0 && index < 16) {
+      setHoraEntrega(horas[index + 1]);
+    }
+  };
+
   return (
     <div>
-      {/* Barra */}
       <div className="top-bar-c">
-        <a href={"/Carro"}><button className="blueBar">
-          <img src={exit} className="exit" alt="Exit" />
-        </button>
+        <a href={"/Carro"}>
+          <button className="blueBar">
+            <img src={exit} className="exit" alt="Exit" />
+          </button>
         </a>
       </div>
     
-      {/* Estado de transacción */}
       <div className="linea"></div>
       <div className="circulos">
         <div className="circuloRelleno" />
@@ -30,13 +48,12 @@ const Detalles = ({ onNext }) => {
       </div>
       <p className="estadoActual">Detalles</p>
 
-      {/* Delivery */}
       <div className="delivery">
         <img src={delivery} className="imgDelivery" alt="Delivery" />
         <p className="horaEntrega">Hora de entrega</p>
-        <div className="hora">
-          <p className="set-hora">2pm</p>
-        </div>
+        <button className="hora" onClick={handleHoraClick}>
+          <p className="set-hora">{horaEntrega}</p>
+        </button>
       </div>
       <button className="confirmar-boton" onClick={onNext}>
         <p className="confirmar">Confirmar</p>
